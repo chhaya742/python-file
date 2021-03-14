c557a93696860ec495a2d789e311170790e6b1de
chhaya742 <76725368+chhaya742@users.noreply.github.com>
2021-03-14 11:38:42 +0530

    Add files via upload

---
+++ odd -even.py
@@ -0,0 +1,10 @@
+a=int(input("enter a number "))
+if a%2==0:
+    if a>10:
+        print("your no. is even or greater than 10")
+    else:
+        print("even or smaller than 10")
+else:
+    print("odd") 
+
+    
